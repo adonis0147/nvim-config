7cf013aa1a0910eccb3a7f4fc1cb22b580f5e779
Adonis Ling <adonis0147@gmail.com>
2022-11-14 14:59:51 +0800

    Fix the key mappings for LuaSnip

--- nvim/lua/plugins/key_bindings.lua
+++ nvim/lua/plugins/key_bindings.lua
@@ -38,55 +38,6 @@ local function setup_telescope_nvim_keymaps()
     vim.keymap.set('n', '<leader>fw', '<cmd>Telescope grep_string<cr>', { noremap = true })
 end
 
-local function setup_luasnip_keymaps()
-    vim.cmd('PackerLoad LuaSnip')
-
-    local luasnip = require('luasnip')
-    local cmp = require('cmp')
-
-    local function t(str)
-        return vim.api.nvim_replace_termcodes(str, true, true, true)
-    end
-
-    local function check_back_space()
-        local col = vim.fn.col('.') - 1
-        if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-            return true
-        else
-            return false
-        end
-    end
-
-    _G.luasnip_expand_or_jump = function(key)
-        if cmp and cmp.visible() then
-            cmp.select_next_item()
-        elseif luasnip and luasnip.expand_or_jumpable() then
-            return t('<plug>luasnip-expand-or-jump')
-        elseif check_back_space() then
-            return t(key)
-        else
-            cmp.complete()
-        end
-        return ''
-    end
-
-    _G.luasnip_jump_prev = function(key)
-        if cmp and cmp.visible() then
-            cmp.select_prev_item()
-        elseif luasnip and luasnip.jumpable(-1) then
-            return t('<plug>luasnip-jump-prev')
-        else
-            return t(key)
-        end
-        return ''
-    end
-
-    vim.keymap.set({ 'i', 's' }, '<tab>', 'v:lua.luasnip_expand_or_jump("<tab>")', { expr = true })
-    vim.keymap.set({ 'i', 's' }, '<s-tab>', 'v:lua.luasnip_jump_prev("<s-tab>")', { expr = true })
-    vim.keymap.set({ 'i', 's' }, '<c-l>', 'v:lua.luasnip_expand_or_jump("<c-l>")', { expr = true })
-    vim.keymap.set({ 'i', 's' }, '<c-h>', 'v:lua.luasnip_jump_prev("<c-h>")', { expr = true })
-end
-
 local function setup_qf_nvim_keymaps()
     local function close_qf_or_buffer()
         local win_info = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
@@ -120,7 +71,6 @@ return {
     setup_hop_nvim_keymaps           = setup_hop_nvim_keymaps,
     setup_kommentary_keymaps         = setup_kommentary_keymaps,
     setup_telescope_nvim_keymaps     = setup_telescope_nvim_keymaps,
-    setup_luasnip_keymaps            = setup_luasnip_keymaps,
     setup_qf_nvim_keymaps            = setup_qf_nvim_keymaps,
     setup_vim_easy_align_keymaps     = setup_vim_easy_align_keymaps,
 }
--- nvim/lua/plugins/settings.lua
+++ nvim/lua/plugins/settings.lua
@@ -135,11 +135,23 @@ end
 local function setup_nvim_cmp()
     vim.opt.completeopt = 'menu,menuone,noselect'
 
+    vim.cmd('PackerLoad LuaSnip')
+
+    local luasnip = require('luasnip')
     local cmp = require('cmp')
+
+    local function has_words_before()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+    end
 
     local function select_next_item(fallback)
         if cmp.visible() then
             cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+        elseif has_words_before() then
+            cmp.complete()
         else
             fallback()
         end
@@ -148,6 +160,8 @@ local function setup_nvim_cmp()
     local function select_prev_item(fallback)
         if cmp.visible() then
             cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
         else
             fallback()
         end
@@ -164,10 +178,10 @@ local function setup_nvim_cmp()
             ['<c-f>'] = cmp.mapping.scroll_docs(4),
             ['<c-e>'] = cmp.mapping.abort(),
             ['<cr>'] = cmp.mapping.confirm({ select = true }),
-            ['<tab>'] = select_next_item,
-            ['<s-tab>'] = select_prev_item,
-            ['<c-l>'] = select_next_item,
-            ['<c-h>'] = select_prev_item,
+            ['<tab>'] = cmp.mapping(select_next_item, { 'i', 's' }),
+            ['<s-tab>'] = cmp.mapping(select_prev_item, { 'i', 's' }),
+            ['<c-l>'] = cmp.mapping(select_next_item, { 'i', 's' }),
+            ['<c-h>'] = cmp.mapping(select_prev_item, { 'i', 's' }),
         }),
         sources = cmp.config.sources({
             { name = 'luasnip' },
@@ -193,8 +207,6 @@ local function setup_nvim_cmp()
             { name = 'cmdline' }
         })
     })
-
-    require('plugins.key_bindings').setup_luasnip_keymaps()
 
     -- Load snippets
     vim.cmd('PackerLoad friendly-snippets')
