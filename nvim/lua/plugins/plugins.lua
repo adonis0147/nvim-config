24f683d5ae9b2817d1e5183655204ab3ee0a6f89
Adonis Ling <adonis0147@gmail.com>
2024-05-21 14:26:40 +0800

    Install jsregexp for L3MON4D3/LuaSnip

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -57,7 +57,7 @@ require('lazy').setup {
     {
         'hrsh7th/nvim-cmp',
         dependencies = {
-            { 'saadparwaiz1/cmp_luasnip',    dependencies = 'L3MON4D3/LuaSnip' },
+            { 'saadparwaiz1/cmp_luasnip',    dependencies = { 'L3MON4D3/LuaSnip', build = 'make install_jsregexp' } },
             { 'hrsh7th/cmp-buffer' },
             { 'hrsh7th/cmp-path' },
             { 'hrsh7th/cmp-cmdline' },
