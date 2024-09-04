1af551aa9e50192bef8d0f4caf2300e5c1732f1b
Adonis Ling <adonis0147@gmail.com>
2024-09-04 14:57:30 +0800

    Add highlights for mfussenegger/nvim-dap

--- nvim/lua/plugins/settings.lua
+++ nvim/lua/plugins/settings.lua
@@ -435,8 +435,39 @@ local function setup_dap()
     }
     for type, icon in pairs(signs) do
         local highlight = 'Dap' .. type
-        vim.fn.sign_define(highlight, { text = icon, texthl = highlight })
+        local config = { text = icon, texthl = highlight }
+        if type == 'Stopped' then
+            config.linehl = highlight .. 'Line'
+            config.numhl = highlight .. 'Line'
+        end
+        vim.fn.sign_define(highlight, config)
     end
+
+    vim.api.nvim_create_autocmd('ColorScheme', {
+        pattern = '*',
+        desc = 'Prevent colorscheme clearing self-defined DAP marker colors',
+        callback = function()
+            -- Reuse current SignColumn background (except for DapStoppedLine)
+            local sign_column_hl = vim.api.nvim_get_hl(0, { name = 'SignColumn' })
+            -- if bg or ctermbg aren't found, use bg = 'bg' (which means current Normal) and ctermbg = 'Black'
+            -- convert to 6 digit hex value starting with #
+            local sign_column_bg = (sign_column_hl.bg ~= nil) and ('#%06x'):format(sign_column_hl.bg) or 'bg'
+            local sign_column_ctermbg = (sign_column_hl.ctermbg ~= nil) and sign_column_hl.ctermbg or 'Black'
+
+            vim.api.nvim_set_hl(0, 'DapBreakpoint',
+                { fg = '#e95678', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+            vim.api.nvim_set_hl(0, 'DapBreakpointCondition',
+                { fg = '#e6db74', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+            vim.api.nvim_set_hl(0, 'DapLogPoint', { fg = '#66d9ef', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+            vim.api.nvim_set_hl(0, 'DapStopped', { fg = '#a6e22e', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+            vim.api.nvim_set_hl(0, 'DapStoppedLine', { bg = '#3d5213', ctermbg = 'Green' })
+            vim.api.nvim_set_hl(0, 'DapBreakpointRejected',
+                { fg = '#8F908A', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+        end
+    })
+
+    -- reload current color scheme to pick up colors override if it was set up in a lazy plugin definition fashion
+    vim.cmd.colorscheme(vim.g.colors_name)
 end
 
 local function setup_nvim_dap_ui()
