6e64337ba775959d232d113ad4f58ce2af8a6a05
Adonis Ling <adonis0147@gmail.com>
2026-04-14 15:16:33 +0800

    refactor: modularize LSP client setup methods

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -4,70 +4,98 @@ vim.pack.add({
 	'https://github.com/mason-org/mason-lspconfig.nvim',
 }, { confirm = false })
 
+local function setup_formatting_method(client, bufnr)
+	if not client:supports_method('textDocument/formatting') then return end
+
+	local group = vim.api.nvim_create_augroup('lsp_formatting_' .. bufnr, { clear = true })
+	vim.api.nvim_create_autocmd('BufWritePre', {
+		group = group,
+		buffer = bufnr,
+		callback = function() vim.lsp.buf.format() end,
+	})
+end
+
+local function setup_inlay_hint_method(client, bufnr)
+	if not client:supports_method('textDocument/inlayHint') then return end
+
+	vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+	vim.api.nvim_create_autocmd('LspProgress', {
+		buffer = bufnr,
+		callback = function()
+			vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+		end
+	})
+end
+
+local function setup_code_lens_method(client, bufnr)
+	if not client:supports_method('textDocument/codeLens') then return end
+
+	vim.lsp.codelens.enable(true, { bufnr = bufnr })
+end
+
+local function setup_document_highlight_method(client, bufnr)
+	if not client:supports_method('textDocument/documentHighlight') then return end
+
+	local visual = vim.api.nvim_get_hl(0, { name = 'Visual' })
+	local opts = { bg = visual.bg, bold = true, underline = true }
+	vim.api.nvim_set_hl(0, 'LspReferenceText', opts)
+
+	local group = vim.api.nvim_create_augroup('lsp_document_highlight_' .. bufnr, { clear = true })
+	vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+		group = group,
+		buffer = bufnr,
+		callback = vim.lsp.buf.document_highlight,
+	})
+
+	vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI', 'BufLeave' }, {
+		group = group,
+		buffer = bufnr,
+		callback = vim.lsp.buf.clear_references,
+	})
+end
+
+local function setup_signature_help_method(client, bufnr)
+	if not client:supports_method('textDocument/signatureHelp') then return end
+
+	local group = vim.api.nvim_create_augroup('lsp_signature_' .. bufnr, { clear = true })
+	local timer = nil
+	local trigger_chars = (
+		client.server_capabilities.signatureHelpProvider and
+		client.server_capabilities.signatureHelpProvider.triggerCharacters
+	) or {}
+
+	if #trigger_chars == 0 then return end
+
+	local function get_last_char()
+		local cursor = vim.api.nvim_win_get_cursor(0)
+		local line = vim.api.nvim_get_current_line()
+		local col = cursor[2]
+		return col > 0 and line:sub(col, col) or ''
+	end
+
+	local function check_signature_help()
+		if vim.tbl_contains(trigger_chars, get_last_char()) then
+			if timer then vim.fn.timer_stop(timer) end
+			timer = vim.fn.timer_start(50, function() vim.lsp.buf.signature_help() end)
+		end
+	end
+
+	vim.api.nvim_create_autocmd({ 'TextChangedI', 'TextChangedP', 'InsertEnter' }, {
+		group = group,
+		buffer = bufnr,
+		callback = check_signature_help,
+	})
+end
+
 local function on_attach(client, bufnr)
 	-- :help lsp-method
 	-- vim.lsp.protocol.Methods
 
-	-- Format code on save.
-	if client:supports_method('textDocument/formatting') then
-		local group = vim.api.nvim_create_augroup('lsp_formatting_' .. bufnr, { clear = true })
-		vim.api.nvim_create_autocmd('BufWritePre', {
-			group = group,
-			buffer = bufnr,
-			callback = function() vim.lsp.buf.format() end,
-		})
-	end
-
-	-- Enable inlay hints
-	if client:supports_method('textDocument/inlayHint') then
-		vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-
-		vim.api.nvim_create_autocmd('LspProgress', {
-			buffer = bufnr,
-			callback = function()
-				vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-			end
-		})
-	end
-
-	-- Enable code lens
-	if client:supports_method('textDocument/codeLens') then
-		vim.lsp.codelens.enable(true, { bufnr = bufnr })
-	end
-
-	-- Enable auto-trigger signature help
-	if client:supports_method('textDocument/signatureHelp') then
-		local group = vim.api.nvim_create_augroup('lsp_signature_' .. bufnr, { clear = true })
-		local timer = nil
-
-		-- Get trigger characters from LSP server
-		local trigger_chars = (
-			client.server_capabilities.signatureHelpProvider and
-			client.server_capabilities.signatureHelpProvider.triggerCharacters
-		) or {}
-
-		if #trigger_chars > 0 then
-			local function get_last_char()
-				local cursor = vim.api.nvim_win_get_cursor(0)
-				local line = vim.api.nvim_get_current_line()
-				local col = cursor[2]
-				return col > 0 and line:sub(col, col) or ''
-			end
-
-			local function check_signature_help()
-				if vim.tbl_contains(trigger_chars, get_last_char()) then
-					if timer then vim.fn.timer_stop(timer) end
-					timer = vim.fn.timer_start(50, function() vim.lsp.buf.signature_help() end)
-				end
-			end
-
-			vim.api.nvim_create_autocmd({ 'TextChangedI', 'TextChangedP', 'InsertEnter' }, {
-				group = group,
-				buffer = bufnr,
-				callback = check_signature_help,
-			})
-		end
-	end
+	setup_formatting_method(client, bufnr)
+	setup_inlay_hint_method(client, bufnr)
+	setup_code_lens_method(client, bufnr)
+	setup_document_highlight_method(client, bufnr)
+	setup_signature_help_method(client, bufnr)
 end
 
 vim.api.nvim_create_autocmd('LspAttach', {
