b0eed79013dfc0b3f6d21c29b38be68fb713248b
Adonis Ling <adonis0147@gmail.com>
2026-04-16 15:13:00 +0800

    feat(nvim-autopairs): add custom double quote pairing rules for specific filetypes

--- nvim/plugin/nvim-autopairs.lua
+++ nvim/plugin/nvim-autopairs.lua
@@ -5,3 +5,10 @@ npairs.setup {
 	check_ts = true,
 	disable_filetype = { 'TelescopePrompt', 'spectre_panel', 'dap-repl' },
 }
+
+local cond = require('nvim-autopairs.conds')
+local quote = require('nvim-autopairs.rules.basic').quote_creator(npairs.config)
+
+npairs.remove_rule('"')
+npairs.add_rule(quote('"', '"', { '-vim', '-sh', '-zsh' }))
+npairs.add_rule(quote('"', '"', 'vim'):with_pair(cond.not_before_regex('^%s*$', -1)))
