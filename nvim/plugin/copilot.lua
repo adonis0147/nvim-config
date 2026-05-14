635eeb1231160731db887a6b0365697a663568a9
Adonis Ling <adonis0147@gmail.com>
2026-05-14 15:45:24 +0800

    fix(copilot.lua): remove unnecessary copilot_node_command

--- nvim/plugin/copilot.lua
+++ nvim/plugin/copilot.lua
@@ -1,5 +1,6 @@
 vim.pack.add({ 'https://github.com/zbirenbaum/copilot.lua' }, { confirm = false })
 
+--- @diagnostic disable-next-line: unused-local, unused-function
 local function nvm_lts_node()
 	local nvm = vim.fn.expand('~/.nvm')
 	if vim.fn.isdirectory(nvm) == 0 then return end
@@ -12,7 +13,7 @@ local function nvm_lts_node()
 end
 
 require('copilot').setup {
-	copilot_node_command = nvm_lts_node(),
+	copilot_node_command = nil,
 	suggestion = {
 		auto_trigger = true,
 	}
